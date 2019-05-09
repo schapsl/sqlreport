8b140783490ae95e628b7f5cb111452b1ce79fcb
schapsl <35762507+schapsl@users.noreply.github.com>
2019-05-09 15:14:04 +0200

    Controller and bean added.

---
+++ SugarModules/modules/msg_SQLReport/controller.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('include/MVC/Controller/SugarController.php');
+
+class msg_SQLReportController extends SugarController {}
--- SugarModules/modules/msg_SQLReport/msg_SQLReport.php
+++ SugarModules/modules/msg_SQLReport/msg_SQLReport.php
@@ -1,12 +1,9 @@
 <?php
-/**
- * THIS CLASS IS FOR DEVELOPERS TO MAKE CUSTOMIZATIONS IN
- */
-require_once('modules/msg_SQLReport/msg_SQLReport_sugar.php');
-class msg_SQLReport extends msg_SQLReport_sugar {
 
+require_once('modules/msg_SQLReport/msg_SQLReport_sugar.php');
+
+class msg_SQLReport extends msg_SQLReport_sugar {
     function __construct(){
         parent::__construct();
     }
-
 }

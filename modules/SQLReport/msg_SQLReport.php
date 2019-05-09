c438fd4a4ec21cdd3a1af0fcd5e3f15fc263be95
Lars <35762507+schapsl@users.noreply.github.com>
2019-05-09 15:05:10 +0200

    Added controller and bean class.

---
+++ modules/SQLReport/msg_SQLReport.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('modules/msg_SQLReport/msg_SQLReport_sugar.php');
+
+class msg_SQLReport extends msg_SQLReport_sugar {
+    function __construct(){
+        parent::__construct();
+    }
+}
